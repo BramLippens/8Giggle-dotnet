81cb1a8614681c72ce298e9e1b0c7bf14fc3114c
BramLippens <bram.lippens09@gmail.com>
2024-01-11 17:57:00 +0100

    init project structure and post

---
+++ src/Domain/Posts/Post.cs
@@ -0,0 +1,34 @@
+﻿using Ardalis.GuardClauses;
+using Domain.Common;
+
+namespace Domain.Posts;
+
+public class Post : Entity
+{
+    private string _title = default!;
+    public string Title
+    {
+        get => _title;
+        set => _title = Guard.Against.NullOrEmpty(value, nameof(Title));
+    }
+
+    private readonly List<Tag> _tags = new();
+    public IReadOnlyList<Tag> Tags => _tags.AsReadOnly();
+
+    private Post() { }
+
+    public Post(string title)
+    {
+        Title = title;
+    }
+
+    public void AddTag(Tag tag)
+    {
+        Guard.Against.Null(tag, nameof(tag));
+        if(Tags.Contains(tag))
+        {
+            throw new ApplicationException($"{nameof(Post)} '{_title}' already contains the tag:{tag.Name}");
+        }
+        _tags.Add(tag);
+    }
+}
---
+++ src/Domain/Posts/Tag.cs
@@ -0,0 +1,20 @@
+﻿namespace Domain.Posts;
+
+public class Tag:Entity
+{
+    private string _name = default!;
+    public string Name
+    {
+        get => _name;
+        set => _name = Guard.Against.NullOrEmpty(value, nameof(Name));
+    }
+
+    private readonly List<Post> _posts = new();
+    public IReadOnlyList<Post> Posts => _posts.AsReadOnly();
+
+    private Tag() { }
+    public Tag(string name)
+    {
+        Name = name;
+    }
+}
